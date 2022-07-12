f58a15391b4d9dd3c0b92de9a1ca6a212127e0c0
Leotrim Mehmetaj <mleotrim@gmail.com>
2022-07-12 11:40:22 -0400

    Added multiple skeleton tests in order to complete 100% code coverage

--- TestATM.java
+++ TestATM.java
@@ -25,6 +25,97 @@ class TestATM {
 		 atm.exchangeCurrency("CAD");
 		 Assert.assertEquals(1300.56, atm.checkBalance(), 0.001);
 		 Assert.assertEquals("CAD", atm.checkCurrency());
-		 
 	 }
+	 
+	 // Handles converting from USD to EUR
+	 @Test 
+	 void testExchangeUSDToEUR() throws Exception {
+		 ATM atm = new ATM(100, "USD");
+		 atm.exchangeCurrency("EUR");
+		 Assert.assertEquals(98, atm.checkBalance(), 0.001);
+		 Assert.assertEquals("EUR", atm.checkCurrency());
+	 }
+	 
+	 @Test
+	 void testExchangeUSDToUnknown() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from CAD to CAD
+	 @Test
+	 void testExchangeCADToCAD() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from CAD to USD
+	 @Test
+	 void testExchangeCADToUSD() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from CAD to EUR
+	 @Test
+	 void testExchangeCADToEUR() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from CAD to an unknown currency
+	 @Test
+	 void testExchangeCADToUnknown() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from EUR to EUR
+	 @Test
+	 void testExchangeEURtoEUR() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from EUR to USD
+	 @Test
+	 void testExchangeEURToUSD() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from EUR to CAD
+	 @Test
+	 void testExchangeEURToCAD() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Handles converting from EUR to an unknown currency
+	 @Test
+	 void testExchangeEURToUnknown() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Test the withdraw functionality with positive values
+	 @Test
+	 void testWithdrawPositive() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Test the withdraw functionality with negative values
+	 @Test
+	 void testWithdrawNegative() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Test the deposit functionality with a positive value
+	 @Test
+	 void testDepositPositive() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 // Test constructor throws error when currency not recognized
+	 @Test
+	 void testCurrencyConstructor() throws Exception {
+		 Assert.assertTrue(true);
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
 }
